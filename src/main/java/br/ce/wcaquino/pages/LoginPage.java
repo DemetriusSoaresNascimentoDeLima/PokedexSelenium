8ebf6835e7dc9823f04e917237cde1c0d1e4a372
DemetriusSoaresNascimentoDeLima <demetrius.lima@ibm.com>
2020-05-27 21:49:28 -0300

    Criei uma classe para abrir o projeto

---
+++ src/main/java/br/ce/wcaquino/pages/LoginPage.java
@@ -0,0 +1,14 @@
+package br.ce.wcaquino.pages;
+
+import br.ce.wcaquino.core.BasePage;
+import br.ce.wcaquino.core.DriverFactory;
+
+public class LoginPage extends BasePage {
+	
+	public void acessarTelaInicial() {
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\DemetriusSoaresNasci\\Desktop\\Imagens\\Tribanco\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		DriverFactory.getDriver().get("http://localhost:4200/");
+	}
+
+}
---
+++ src/main/java/br/ce/wcaquino/test/PokemonList.java
@@ -0,0 +1,15 @@
+package br.ce.wcaquino.test;
+
+import org.junit.Test;
+
+
+import br.ce.wcaquino.core.BaseTest;
+
+public class PokemonList extends BaseTest {
+	
+	@Test
+	public void testVerDetalhes() {
+		
+	}
+
+}
